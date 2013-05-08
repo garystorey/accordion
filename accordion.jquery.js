ee67c25487c388bba29aee7b96511fc7cf87a710
Gary Storey <garystorey@gmail.com>
2013-05-08 12:55:48 -0300

    Updated private routines to start with an underscore

--- accordion.jquery.js
+++ accordion.jquery.js
@@ -13,34 +13,34 @@ var Accordion = function( $elm, options ) {
 Accordion.prototype = {
 
   init : function () {
-    this.addButtons();
-    this.eventBindings();
+    this._addButtons();
+    this._eventBindings();
     this.reset();
-    this.log(this.$el);
-    this.log(this.options);
+    this._log(this.$el);
+    this._log(this.options);
 
   },
 
   destroy : function () {
-    this.log('Destroy!');
+    this._log('Destroy!');
     this.$el.off('click.accordion')
       .find( '.' + this.options.collapseAndExpand.expandAllClass ).remove().end()
       .find( '.' + this.options.collapseAndExpand.collapseAllClass ).remove().end();
   },
 
   reset : function () {
-    this.log('Reseting Accordion to default state...');
-    this.collapseAll();
+    this._log('Reseting Accordion to default state...');
+    this._collapseAll();
     if ( this.options.showFirstArea ) {
       this.$parents.first().addClass( this.options.expandClass );
     }    
   },
 
-  log : function ( txt ) {
+  _log : function ( txt ) {
     if ( this.debug ) { console.log( txt ); }
   },
 
-  clickHandler : function( event ) {
+  _clickHandler : function( event ) {
 
     var $el = jQuery( event.target ),
         $p = $el.parent(),
@@ -49,38 +49,38 @@ Accordion.prototype = {
     
     if ( $el.hasClass( opts.collapseAndExpand.expandAllClass ) ) {
       $el.trigger('accordion-expand-all');
-      obj.expandAll(); obj.isOneButton($el); return;
+      obj._expandAll(); obj._isOneButton($el); return;
     }
 
     if ( $el.hasClass( opts.collapseAndExpand.collapseAllClass ) ) {
       $el.trigger('accordion-collapse-all');
-      obj.collapseAll(); obj.isOneButton($el); return;
+      obj._collapseAll(); obj._isOneButton($el); return;
     }
 
     if ( $p.hasClass( opts.expandClass ) && !opts.oneAreaAlwaysVisible ) {
       $el.trigger('accordion-click');
-      obj.itemClick( $el );return;
+      obj._itemClick( $el );return;
     }
 
     if ( $el.hasClass( opts.headerClass ) ) {
-      obj.allowMultipleAreas();
+      obj._allowMultipleAreas();
       $el.trigger('accordion-click');
-      obj.itemClick( $el );return;
+      obj._itemClick( $el );return;
     }
   },
 
-  allowMultipleAreas : function() {
+  _allowMultipleAreas : function() {
     if ( !this.options.showMultipleAreas ) {
-      this.log('   - Multiples arent allowed.');
-      this.log('   - Collapsing Items.');
-      this.collapseAll();
+      this._log('   - Multiples arent allowed.');
+      this._log('   - Collapsing Items.');
+      this._collapseAll();
     }
   },
 
-  isOneButton : function ($el) {
-    this.log('   - Is this One Button? ' + this.options.collapseAndExpand.showAsOneButton);
+  _isOneButton : function ($el) {
+    this._log('   - Is this One Button? ' + this.options.collapseAndExpand.showAsOneButton);
     if ( this.options.collapseAndExpand.showAsOneButton ) {
-      this.log('   - Switching button context.');
+      this._log('   - Switching button context.');
       if (! $el.hasClass( this.options.collapseAndExpand.expandAllClass ) ) {
         $el.removeClass( this.options.collapseAndExpand.collapseAllClass ).addClass( this.options.collapseAndExpand.expandAllClass ).text( this.options.collapseAndExpand.expandAllText );
         return;
@@ -89,36 +89,36 @@ Accordion.prototype = {
     }
   }, 
 
-  eventBindings : function () {
+  _eventBindings : function () {
     var self = this;
-    this.log('Binding Events...');
-    this.$el.on( 'click.accordion' , self, self.clickHandler );
+    this._log('Binding Events...');
+    this.$el.on( 'click.accordion' , self, self._clickHandler );
   },
 
 
-  addButtons : function() {
+  _addButtons : function() {
     var expand, collapse;
     if ( this.options.showCollapseAndExpand ) {
-      this.log('Adding Expand All Button...');
+      this._log('Adding Expand All Button...');
       expand = jQuery('<span/>', {'class' : this.options.collapseAndExpand.expandAllClass, 'text': this.options.collapseAndExpand.expandAllText});
-      this.log('Add Collpase All button?');
-      this.log(( !this.options.collapseAndExpand.showAsOneButton )? 'No.' : 'Yes.');
+      this._log('Add Collpase All button?');
+      this._log(( !this.options.collapseAndExpand.showAsOneButton )? 'No.' : 'Yes.');
       collapse = ( !this.options.collapseAndExpand.showAsOneButton ) ? jQuery('<span/>', {'class' : this.options.collapseAndExpand.collapseAllClass, 'text': this.options.collapseAndExpand.collapseAllText}) : '';
       $(this.$el).prepend( expand, collapse ); 
     }
     expand=null; collapse=null;
   },
 
-  itemClick : function ( $el ) {
+  _itemClick : function ( $el ) {
       var $p = $el.parent();
       $p.toggleClass( this.options.expandClass );
   },
 
-  expandAll : function () {
+  _expandAll : function () {
     this.$parents.addClass( this.options.expandClass );
   },
 
-  collapseAll : function () {
+  _collapseAll : function () {
     this.$parents.removeClass( this.options.expandClass );
   }
 
